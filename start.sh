63a9395041f23ccddaebe44c40d001ced1cc6766
Matthieu Fronton <fronton@ekino.com>
2016-02-19 00:41:57 +0100

    wait longer for cluster to sync after docker engine restart

--- start.sh
+++ start.sh
@@ -24,7 +24,7 @@ function start_ucp {
 
   # wait for all docker engines to restart and sync with 
   # their new multi-host networking setup (can take some time...)
-  sleep 30
+  sleep 90
 
   cd ucp/bundle/
   source env.sh
