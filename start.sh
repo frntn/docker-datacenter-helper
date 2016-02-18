7b6b24931f64b8f547b77870caf6bf4bf31cb309
Matthieu Fronton <fronton@ekino.com>
2016-02-18 05:38:14 +0100

    add orchestrator script

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -u
+
+cd "$(dirname "$0")"
+
+function start_ucp {
+  set -x
+  time vagrant destroy controller node1 node2 -f > out.log
+  time vagrant up controller >> out.log
+  time ./ucp/helper.sh >> out.log
+  set +x
+
+  cd ucp/bundle/
+  source env.sh
+  docker version
+  docker info
+}
+
+case "$1" in
+  'ucp'|'dtr') main="start_$1";;
+esac
+
+$main
